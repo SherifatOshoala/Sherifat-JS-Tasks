fd61a44b05b194eb45d06d6d165c1962ac0c71ee
Sherifat Oshoala <Pelumioshoala@gmail.com>
2024-03-27 06:38:58 +0100

    corrected = to ===

--- Sherifat-3.js
+++ Sherifat-3.js
@@ -1,10 +1,21 @@
-
-
-let no1 = 100
+let no1 = -100;
 
 if (no1 > 0) {
   console.log("Positive");
-} else if (no1 = 0) {
+} else if (no1 === 0) {
+  console.log("Zero");
+} else {
+  console.log("Negative");
+}
+
+// OR math.sign
+
+let no1b = -20;
+let no1b2 = Math.sign(no1b);
+
+if (no1b2 === 1) {
+  console.log("Positive");
+} else if (no1b2 === 0) {
   console.log("Zero");
 } else {
   console.log("Negative");
@@ -12,14 +23,14 @@ if (no1 > 0) {
 
 // OR string the number
 
-// let no1c = "100";
-// if (no1c.startsWith("-") === true) {
-//   console.log("Negative");
-// } else if (no1 === 0) {
-//   console.log("Zero");
-// } else {
-//   console.log("Positive");
-// }
+let no1c = "-100";
+if (no1c.startsWith("-") === true) {
+  console.log("Negative");
+} else if (no1c === 0) {
+  console.log("Zero");
+} else {
+  console.log("Positive");
+}
 
 let no2 = 23;
 if (no2 % 2 === 0) {
@@ -49,14 +60,16 @@ if (year >= 1901 && year <= 2000) {
   console.log("none");
 }
 
-let a = 11;
+let a = 90;
+let b;
 
 if (a < 10) {
-  console.log(0);
+  b = 0;
 } else if (a >= 10 && a <= 50) {
-  console.log(1);
+  b = 1;
 } else if (a >= 51 && a <= 100) {
-  console.log(2);
+  b = 2;
 } else {
-  console.log(3);
+  b = 3;
 }
+console.log('b =', b);
